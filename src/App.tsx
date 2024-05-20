9183a998b293dcb78c1441234dc35caad79bd299
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-05-21 01:45:20 +0530

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Loader from "./components/Loader";
 import Navbar from "./components/Navbar";
 import Routing from "./Routing";
 
 function App() {
   const [milkingSession , setMilkingSession] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  },[]);
+
   return (
     <div id="main" className="bg-gradient-to-r from-blue-500 to-pink-500 w-full h-[100vh]">
+      {loading  && <Loader />} 
       <Navbar milkingSession={milkingSession} />
       <Routing setMilkingSession={setMilkingSession} />
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,31 @@
-import useSound from "use-sound"
-import MainPage from "./MainPage"
-import SoothingAudio from "../assets/soothing.mp3"
-import { MilkinSession } from "../utils/modals/Milking.modal"
+import useSound from "use-sound";
+import MainPage from "./MainPage";
+import SoothingAudio from "../assets/soothing.mp3";
+import { MilkinSession } from "../utils/modals/Milking.modal";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
 
-const Home = ({setMilkingSession} : MilkinSession) => {
-    const [play , { pause , stop }] = useSound(SoothingAudio)
+const Home = ({ setMilkingSession }: MilkinSession) => {
+  const [play, { pause, stop }] = useSound(SoothingAudio);
+ 
+
   return (
     <div className="flex flex-col items-center justify-center px-4 ">
-    <div className="text-3xl text-center mt-40">
-      Welcome to Milking Tracker
-    </div>
-    <div></div>
-    <div className="mt-30">
-      <MainPage setMilkingSession={setMilkingSession} play={play} pauseSound={pause} stop={stop} />
-    </div>
-  </div>
-  )
-}
 
-export default Home
+      <div className="text-3xl text-center mt-40">
+        Welcome to Milking Tracker
+      </div>
+      <div></div>
+      <div className="mt-30">
+        <MainPage
+          setMilkingSession={setMilkingSession}
+          play={play}
+          pauseSound={pause}
+          stop={stop}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
